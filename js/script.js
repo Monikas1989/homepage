dbc578d6a720c7d79b7b740aa29bf9f87f9bf85c
Monika Kazula <lorika09022014@gmail.com>
2022-02-17 20:26:04 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("Witam!");
+let przycisk = document.querySelector (".przycisk");
+let podtytul3 = document.querySelector (".podtytul3");
+przycisk.addEventListener("click" , () =>  {   
+podtytul3.remove();});
+
