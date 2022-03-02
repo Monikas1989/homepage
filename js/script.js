77b288434b2989740e4884a19dda25721f7c3f91
Monika Kazula <lorika09022014@gmail.com>
2022-03-02 14:57:32 +0100

    code formated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 {
-const button = document.querySelector(".button");
-const subtitle3 = document.querySelector(".subtitle3");
-button.addEventListener("click", () => {
-    subtitle3.remove();
-});
-const init = () => {
-    console.log("Hello, welcome!");
-};
-init();
+    const button = document.querySelector(".button");
+    const subtitle3 = document.querySelector(".subtitle3");
+    button.addEventListener("click", () => {
+        subtitle3.remove();
+    });
+    const init = () => {
+        console.log("Hello, welcome!");
+    };
+    init();
 }
 
 
